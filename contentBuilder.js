9ac6c80c03b4305b35493fdf0ae187bb927daf12
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-10-12 23:29:50 -0400

    defaultImg option for contentBuilder

--- contentBuilder.js
+++ contentBuilder.js
@@ -5,10 +5,15 @@ module.exports = {
 	//accepts a data source and options
 	contentBuilder: (
 		data,
-		options = { width: 'auto', numHeaderRows: 1, style: tableStyle }
+		options = {
+			width: 'auto',
+			numHeaderRows: 1,
+			style: tableStyle,
+			defaultImg: 'img/test.jpg',
+		}
 	) => {
 		//retrieve any user given options, otherwise us the defaults
-		let { width, numHeaderRows, style } = options;
+		let { width, numHeaderRows, style, defaultImg } = options;
 		let builtHeader = false;
 
 		//build the table that houses the data
@@ -24,27 +29,42 @@ module.exports = {
 		//build the body of the table
 		let body = data.map((i) => {
 			let arr = [];
-			i.img = `img/${i.csk_sku}.jpg`;
+
+			//check if already an image propery, if not set it
+			if (!i.img) i.img = `img/${i.csk_sku}.jpg`;
+
+			//loop through each row's columns
 			for (property in i) {
+				//if we haven't built the header, build it
 				if (!builtHeader) {
 					console.log(property);
 					table.widths.push(width || 'auto');
 					headerContent.push({ text: property });
 				}
-
+				//if the current property is image, insert the image object
 				if (property === 'img') {
-					if (!fs.existsSync(i.img)) i.img = 'img/test.jpg';
+					//if the image path doesn't exist, set it to a default
+					if (!fs.existsSync(i.img)) i.img = defaultImg;
+
+					//read it as binary
 					var bin = fs.readFileSync(i.img);
+
+					//create base64 encoded string
 					var b64 = Buffer.from(bin).toString('base64');
 
+					//push the imagestring into the array as an image type
 					arr.push({
 						image: `data:image/jpeg;base64,${b64}`,
 					});
 				} else {
+					//otherwise just push it in as text
 					arr.push({ text: i[property] });
 				}
 			}
+			//set the header to true after the first time around
 			builtHeader = true;
+
+			//push the array into the body
 			return arr;
 		});
 		console.log(table.widths);
